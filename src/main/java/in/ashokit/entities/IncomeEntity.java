9e3c6296ef438aa36a23006ce6ca4931ece39acf
rajukorivi1507-wq <rajukorivi1507@gmail.com>
2026-07-01 16:07:12 +0530

    DCIncomeEntity.java

--- src/main/java/in/ashokit/entities/IncomeEntity.java
+++ src/main/java/in/ashokit/entities/IncomeEntity.java
@@ -1,5 +1,14 @@
 package in.ashokit.entities;
 
-public class IncomeEntity {
+  @Entity
+  @Table(name="DC_INCOME")
+  @Data
+public class DCIncomeEntity {
 
+  @Id
+  @GeneratedValue
+  private Integer incomeId;
+  private Long caseNum;
+  private Double empIncome;
+  private Double propertyIncome;
 }
