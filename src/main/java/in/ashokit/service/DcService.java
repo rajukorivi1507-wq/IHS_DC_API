d7069e337f24183cc2830fd31d7a04ca7e2db2b7
rajukorivi1507-wq <rajukorivi1507@gmail.com>
2026-07-01 17:47:49 +0530

    DcService.java

--- src/main/java/in/ashokit/service/DcService.java
+++ src/main/java/in/ashokit/service/DcService.java
@@ -29,3 +29,60 @@ public interface DcService {
 	public DcSummary fetchSummaryInfo(Long caseNum);
 
 }
+
+public Class DcServiceImpl implements DCservice  {
+
+	public Long loadCasenum(Intefer appId){
+		
+      // DCaseRepo => public DcaseEntity findByAppId(Integer appId);
+		
+		DcCaseEntity findById= dccaseRepo.findByAppId(appId);
+	    if(findById!=null){
+			return findByAppId.getCaseNum();
+		}
+	return null;
+	}
+	public List<String> getPlanNames{
+		
+	}
+	public Long savePlanSelection(PlanSelection planselection){
+		
+	}
+	public Long saveIncomeData(Income income){
+		
+	}
+	public Long saveEducation(Education education){
+		
+	}
+	public Long saveChildrens(List<Child> childs){
+		
+	}
+	public DcSummary getSummary(Long caseNumber){
+		
+	}
+
+
+	
+   ======================================================
+	public Map<Integer, String> getPlans();
+	public boolean savePlanSelection(PlanSelection planSel);
+	public boolean saveIncome(Income income);
+	public boolean saveEducation(Education education);
+	public boolean saveKids(Kids kids);
+	public DcSummary fetchSummaryInfo(Long caseNum);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
