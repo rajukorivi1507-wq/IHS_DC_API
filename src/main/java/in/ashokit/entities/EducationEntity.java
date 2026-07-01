c3dbe5a74a47d0fbc8be1d0904d05bf7e738b759
rajukorivi1507-wq <rajukorivi1507@gmail.com>
2026-07-01 16:13:07 +0530

    DcEducationEntity.java

--- src/main/java/in/ashokit/entities/EducationEntity.java
+++ src/main/java/in/ashokit/entities/EducationEntity.java
@@ -1,5 +1,15 @@
 package in.ashokit.entities;
 
-public class EducationEntity {
+@Entity
+  @Table(name="DCEducationEntity")
+  @Data
+public class DCEducationEntity {
 
+  @Id
+  @GeneratedValue 
+  private Integer eduId;
+  private Long caseNum;
+  private String highestQualification;
+  private Integer graduationYear;
+  
 }
